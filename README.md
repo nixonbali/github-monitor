8c8dbd20c4a8cc67028843a199870ac8852e11dd
nixonbali <nixon.ubud@gmail.com>
2020-01-21 20:25:27 -0500

    modularize + cli - api_to_s3

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # GitHub Events Monitor
 
 Using the [GH Archive](https://www.gharchive.org/) dataset of events on the public GitHub timeline, the goal of this project is to build real-time ingestion and processing application with Kafka to monitor topics, contributions, repositories, and collaborations of interest.
+
+## Ingestion
+The [GH Archive](https://www.gharchive.org/) is available for download via an API, however to mitigate the risk of the streaming directly from the API during my Kafka ingestion, I'm also storing all of this data in an S3 bucket. This process is done with `api_to_s3.py` which takes dates as command line date arguments. This is so that it can be run across different subsets of dates from the shell on multiple EC2 instances in parallel to speed up ingestion. The only requirements are that the `awscli` is configured with your credentials on each instance and python3 is installed with the necessary packages (e.g. `boto3`). Additionally, a file `logs/gh-events.log` in your directory and an S3 bucket already built and named `git-events` will allow the script to be run without modifications.
--- ingestion/api_to_s3.py
+++ ingestion/api_to_s3.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 import time
 import boto3
@@ -10,18 +10,9 @@ archive_domain = "https://data.gharchive.org/"
 zip_file_ext = ".gz"
 json_file_ext = ".json"
 
-### S3 BUCKET
+### S3 CLIENT
 s3_client = boto3.client('s3')
-s3bucket = "git-events"
 
-### LOGGING
-starttime = prevtime = time.time()
-with open("logs/gh-events.log", "w") as logfile:
-	logfile.write("Time\t\t\tTime Since Last\t\tTime Since Start\t\t\t")
-
-### DATE RANGE
-start_date = datetime.datetime(2012,1,1,0)
-end_date = datetime.datetime(2020,1,19,0)
 # source:
 # https://stackoverflow.com/questions/1060279/iterating-through-a-range-of-dates-in-python
 def daterange(start_date, end_date):
@@ -29,8 +20,24 @@ def daterange(start_date, end_date):
         yield start_date + datetime.timedelta(n)
 ######
 
+### DATA EXTRACT TO LOCAL
+def api_to_local(filename):
+	zip_filename = filename + zip_file_ext
+	url = archive_domain + zip_filename
+	## shell script
+	subprocess.run(["wget", url])
+	subprocess.run(["gzip", zip_filename, "-d"])
 
-try:
+
+
+### API -> S3
+def hourly_events(start_date, end_date, s3bucket="git-events", logging=True):
+	### INITIATE LOG
+	if logging:
+		starttime = prevtime = time.time()
+		with open("logs/gh-events.log", "w") as logfile:
+			logfile.write("Time\t\t\tTime Since Last\t\tTime Since Start\t\t\t\n")
+
 	### BY HOUR
 	for singledate in daterange(start_date, end_date):
 		for singlehour in range(24):
@@ -38,12 +45,7 @@ try:
 			###### EXTRACT DATA
 			datestring = ((singledate + datetime.timedelta(hours=singlehour)).strftime("%Y-%m-%d-%-H"))
 			filename = datestring + json_file_ext
-			zip_filename = filename + zip_file_ext
-			url = archive_domain + zip_filename
-
-			## shell script
-			subprocess.run(["wget", url])
-			subprocess.run(["gzip", zip_filename, "-d"])
+			api_to_local(filename)
 
 			######## UPLOAD TO S3
 			object_name = "git-events-" + datestring
@@ -53,6 +55,7 @@ try:
 			except Exception:
 				print("error: {}".format(e))
 
+			### RM FROM LOCAL
 			## shell script
 			subprocess.run(["rm", filename])
 
@@ -61,13 +64,27 @@ try:
 			####### LOG TIME
 			with open("logs/gh-events.log", "a+") as logfile:
 				t = time.time()
-				logfile.write("{}\t{}\t{}".format(t, t-prevtime, t-starttime))
+				logfile.write("{}\t{}\t{}\n".format(t, t-prevtime, t-starttime))
 				prev_time = t
 
+if __name__ == "__main__":
+	### DATE RANGE
+	if len(sys.argv) == 1:
+		start_date = datetime.datetime(2012,1,1,0)
+		end_date = datetime.datetime(2020,1,19,0)
+	elif len(sys.argv) == 4:
+		start_date = datetime.datetime(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), 0)
+		end_date = datetime.datetime(2020,1,19,0)
+	elif len(sys.argv) == 7:
+		start_date = datetime.datetime(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), 0)
+		end_date = datetime.datetime(int(sys.argv[4]), int(sys.argv[5]), int(sys.argv[6]), 0)
+	else:
+		print("Invalid Arguments: Must be None, 3, or 6.\nstart_date: YYYY M D\nend_date: YYYY M D")
+		sys.exit(0)
 
-
-
-except KeyboardInterrupt:
-	None
-
-sys.exit(0)
+	### RUN
+	try:
+		hourly_events(start_date, end_date, s3bucket="git-events")
+	except KeyboardInterrupt:
+		None
+	sys.exit(0)
