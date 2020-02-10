ac11c8d7f9ec2d3f9247f42f1e32cf6c6074fb11
nixonbali <nixon.ubud@gmail.com>
2020-02-10 13:50:11 -0500

    unifies flask apps

--- flask/app.py
+++ flask/app.py
@@ -1,14 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from secrets import POSTGRES
-from pr_model import PRModel, PRSchema, rdb, ma
+from secrets import POSTGRES, NEO4J_HOST
+from models.pr_model import PRModel, PRSchema, rdb, ma
 import requests
 import json
+import neomodel
+from grest import global_config
+from views.users_view import UsersView
+from views.repos_view import ReposView
 
 
 app = Flask(__name__)
+
+"""Connection to Postgres"""
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 rdb.init_app(app)
 ma.init_app(app)
+
+"""Connection to Neo4j"""
+# configure connection to database
+neomodel.config.DATABASE_URL = NEO4J_HOST # The bolt URL of your Neo4j instance
+neomodel.config.AUTO_INSTALL_LABELS = True
+neomodel.config.FORCE_TIMEZONE = True  # default False
+
 
 pr_schema = PRSchema()
 prs_schema = PRSchema(many=True)
@@ -42,7 +55,7 @@ def process():
         metrics['merge_rate'] = merge_rate
 
         mean_open_time = requests.get("http://0.0.0.0:5000" + url_for('repo_pr_time',user=username,repo=repo)).json()
-        print(mean_open_time)
+        #print(mean_open_time)
         metrics.update(mean_open_time)
 
 
@@ -87,6 +100,10 @@ def repo_pr_time(user, repo):
         'hours': pr_time.seconds // 3600,
         'minutes': (pr_time.seconds % 3600) / 60,
       }
+
+"""Neo4j Views"""
+UsersView.register(app, route_base="/users", trailing_slash=False)
+ReposView.register(app, route_base="/repos", trailing_slash=False)
 
 if __name__ == "__main__":
     app.config['DEBUG'] = True
